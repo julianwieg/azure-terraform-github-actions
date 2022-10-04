a6ae7137440d7eb93ee702fedc283b344c679a33
Julian Wiegmann <julianwieg@users.noreply.github.com>
2022-10-04 23:04:49 +0200

    Update main.tf
    terraform {
      required_providers {
        azurerm = {
          source  = "hashicorp/azurerm"
          version = "=3.0.0"
        }
      }
    }
    
    # Configure the Microsoft Azure Provider
    provider "azurerm" {
      features {}
    }

--- main.tf
+++ main.tf
@@ -1,18 +1,15 @@
-# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.25.0"
+      version = "=3.0.0"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  version = "3.25.0"
 }
 
 resource "azurerm_resource_group" "rg" {
